b473d9b796f6aa2468f106efa964647534831520
Abhi4995 <54199553+Abhi4995@users.noreply.github.com>
2019-08-17 12:26:48 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First function makeCacheMatrix is used to set a cache value of matrix ( n this case invertable), 
+##  which is used in the next function to calcucate the inverse of a cached matrix
 
-## Write a short comment describing this function
+
+## makeCacheMatrix returns a list containing functions to
+##              1. set the matrix
+##              2. get the matrix
+##              3. set the inverse
+##              4. get the inverse
+##         this list is used as the input to the function cacheSolve()
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inv <- NULL
+  set <- function(y) {
+    # `<<-` is used to assign a value to an object in an environment 
+    # different from the current environment. Setting up a cached value.
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  
+  setinv <- function(inverse) inv <<- inverse 
+  
+  getinv <- function() inv
+  
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  
+  
+  
 }
 
 
-## Write a short comment describing this function
+## This function return a matrix, which is inverse of the called matrix.
+## Takes input from the function makeCacheMatrix and marks the inverse of the cached value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Returns a matrix that is the inverse of 'x' taken from makeCacheMatrix
+       
+        inv <- x$getinv  ## takes value for inverse from makeCacheMatrix
+        
+        if (!is.na(inv))
+        {
+          inv  ##value taken from cache if inverse exists
+          
+        }
+        
+        ##calculating inverse if above if statement is false
+        
+        matrix.data <- x$get()
+        
+        inv <- solve(matrix.data,...)
+        
+        x$setinv(inv) ##this function sets value of inverse in cache for future use if any.
+        
+        inv
+        
+  
+  
 }
